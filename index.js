5f0b374bed3055ce46fe164ad5e89d6263a9ec7f
Mohammad Tollabi <mohammad.t.0290@gmail.com>
2021-12-28 14:09:47 +0330

    feat: Add body parser

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// Body Parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
+// API routes
 app.use("/api", require("./src/routes"));
 
 app.listen(PORT, () => console.log(`> server started on port ${PORT}`));
