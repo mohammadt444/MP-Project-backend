0e3399dd78b98a132be7be2b061f41bb4278077a
Mohammad Tollabi <mohammad.t.0290@gmail.com>
2021-12-29 18:10:37 +0330

    feat: Add advertisements handler

--- src/handlers/advertisments.js
+++ src/handlers/advertisments.js
@@ -1,1 +1,13 @@
-export default handler = (req, res) => {};
+import {
+  findAdvertisementByBookId,
+  getAdvertisements,
+} from "../db/model/Advertisement";
+
+export default (req, res) => {
+  const { id: BookId } = req.body;
+  if (BookId) {
+    findAdvertisementByBookId(BookId).then((data) => res.json(data));
+  } else {
+    getAdvertisements().then((data) => res.json(data));
+  }
+};
