adf36c3b76f2ec22a0b83e687626db64964ce745
Mohammad Tollabi <mohammad.t.0290@gmail.com>
2021-12-29 18:31:58 +0330

    feat: Add routers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,18 @@
 import express from "express";
+
 import bookHandler from "../handlers/book.js";
+import advertisementsHandler from "../handlers/advertisments.js";
 
 const router = express.Router();
 
-router.get("/advertisements", (req, res) => res.send("advertisements"));
+router.get("/advertisements", advertisementsHandler);
 
-router.post("/book", bookHandler);
+router.post("/advertisement");
+
+router.get("/book", bookHandler);
+
+router.post("/books", bookHandler);
+
+router.get("/bookTitles", bookTitlesHandler);
 
 export default router;
